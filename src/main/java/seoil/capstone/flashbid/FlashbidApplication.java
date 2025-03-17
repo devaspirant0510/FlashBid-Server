3c9296b8b418e0817692eab56cf1fcbac6ec994c
devaspirant0510 <seungho020510@gmail.com>
2025-03-18 00:13:39 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/seoil/capstone/flashbid/FlashbidApplication.java
@@ -0,0 +1,13 @@
+package seoil.capstone.flashbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashbidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashbidApplication.class, args);
+	}
+
+}
