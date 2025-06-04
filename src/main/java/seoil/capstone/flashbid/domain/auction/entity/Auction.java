335121f6f6e802a98ea8f52fde8ef16f9b272348
devaspirant0510 <seungho020510@gmail.com>
2025-06-05 00:58:16 +0900

    fix: db 구조 변경

--- src/main/java/seoil/capstone/flashbid/domain/auction/entity/Auction.java
+++ src/main/java/seoil/capstone/flashbid/domain/auction/entity/Auction.java
@@ -1,22 +1,30 @@
 package seoil.capstone.flashbid.domain.auction.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.*;
 import seoil.capstone.flashbid.domain.user.entity.Account;
+import seoil.capstone.flashbid.global.common.enums.AuctionType;
 import seoil.capstone.flashbid.global.core.BaseTimeEntity;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Auction extends BaseTimeEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
     private Account user;
+
+    @ManyToOne
+    private Goods goods;
 
     @Column
     private int viewCount;
@@ -35,6 +43,9 @@ public class Auction extends BaseTimeEntity {
 
     @Column
     private int count;
+
+    @Enumerated
+    private AuctionType auctionType;
 
 
 
