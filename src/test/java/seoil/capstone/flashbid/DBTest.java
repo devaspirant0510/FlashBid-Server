c983f2ec2e79757f7ae97201609f357d6fba74c3
devaspirant0510 <seungho020510@gmail.com>
2025-08-05 23:10:24 +0900

    test: test code 변경

--- src/test/java/seoil/capstone/flashbid/DBTest.java
+++ src/test/java/seoil/capstone/flashbid/DBTest.java
@@ -25,22 +25,22 @@ public class DBTest {
     private HashProvider hashProvier;
     @Test
     public void insertUserData() {
-        long expirationMillis = 1000L * 60 * 60 * 24 * 365; //
-        Date now = new Date();
-        Date exp = new Date(now.getTime() + expirationMillis);
-
-        SecretKey signedKey = hashProvier.getSignedKey("3bd56aea9642eff83cb0e2fb20d95b4e3bd56aea9642eff83cb0e2fb20d95b4e");
-        String jwt = Jwts.builder()
-                .setSubject("b4ff0229451d8fc4f98b0c2a6545da33")
-                .setIssuedAt(now)
-                .setExpiration(exp)
-                .claim("id", "1")
-                .claim("uid", "b4ff0229451d8fc4f98b0c2a6545da33")
-                .claim("email", "1234")
-                .claim("role", "1234")
-                .signWith(signedKey, SignatureAlgorithm.HS256)
-                .compact();
-        System.out.println(jwt);
+//        long expirationMillis = 1000L * 60 * 60 * 24 * 365; //
+//        Date now = new Date();
+//        Date exp = new Date(now.getTime() + expirationMillis);
+//
+//        SecretKey signedKey = hashProvier.getSignedKey("3bd56aea9642eff83cb0e2fb20d95b4e3bd56aea9642eff83cb0e2fb20d95b4e");
+//        String jwt = Jwts.builder()
+//                .setSubject("b4ff0229451d8fc4f98b0c2a6545da33")
+//                .setIssuedAt(now)
+//                .setExpiration(exp)
+//                .claim("id", "1")
+//                .claim("uid", "b4ff0229451d8fc4f98b0c2a6545da33")
+//                .claim("email", "1234")
+//                .claim("role", "1234")
+//                .signWith(signedKey, SignatureAlgorithm.HS256)
+//                .compact();
+//        System.out.println(jwt);
 //        Account account = new Account(LoginType.KAKAO, UserStatus.ACTIVE, UserType.CUSTOMER, "seungho020510@gmail.com", LocalDateTime.now(), LocalDateTime.now(), true, "1000");
 //        accountRepository.save(account);
     }
