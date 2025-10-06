397c27953ecb33011f4dc25a664eb5cb72d84704
devaspirant0510 <seungho020510@gmail.com>
2025-10-07 00:19:59 +0900

    feat: 리프레시 토큰 쿠키 생성 및 제거 기능 추가

--- src/main/java/seoil/capstone/flashbid/global/core/provider/CookieProvider.java
+++ src/main/java/seoil/capstone/flashbid/global/core/provider/CookieProvider.java
@@ -1,9 +1,11 @@
 package seoil.capstone.flashbid.global.core.provider;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class CookieProvider {
-   // @Value("${MODE}")
- //   private String mode;
+    public static final String REFRESH_TOKEN = "refresh_token";
+    private final JwtProvider jwtProvider;
+    @Value("${MODE}:production")
+    private String mode;
 
+    @Deprecated
     public Cookie generateCookie(String key, String value, int time) {
         Cookie cookie = new Cookie(key, value);
         cookie.setHttpOnly(true);
@@ -22,4 +27,29 @@ public class CookieProvider {
         cookie.setMaxAge(time);
         return cookie;
     }
+
+    public ResponseCookie generateRefreshTokenCookie(String refreshToken) {
+        boolean isProd = mode.equals("production");
+        Claims claims = jwtProvider.parseClaims(refreshToken);
+        // 만료시간 - 현재시간 = 쿠키 만료시간
+        long exp = claims.getExpiration().getTime()/1000;
+        int cookieMaxAge = (int)(exp - (System.currentTimeMillis()/1000));
+
+        return ResponseCookie.from(REFRESH_TOKEN, refreshToken)
+                .httpOnly(true)
+                .secure(isProd) // 운영 환경에서는 true로 설정
+                .path("/")
+                .maxAge(cookieMaxAge)
+                .sameSite(isProd?"None":"Lax")
+                .build();
+    }
+    public Cookie removeCookie(String key) {
+        Cookie cookie = new Cookie(key, null);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setSecure(true);
+        cookie.setMaxAge(0);
+        return cookie;
+    }
+
 }
