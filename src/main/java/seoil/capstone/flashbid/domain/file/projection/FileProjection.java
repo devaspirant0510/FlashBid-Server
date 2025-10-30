8c67da2b3aeb0e09e7ffe1503a217d71a730f75c
devaspirant0510 <seungho020510@gmail.com>
2025-10-31 02:36:38 +0900

    feat: 파일 프로젝션에 파일 ID 및 파일 타입 추가

--- src/main/java/seoil/capstone/flashbid/domain/file/projection/FileProjection.java
+++ src/main/java/seoil/capstone/flashbid/domain/file/projection/FileProjection.java
@@ -1,6 +1,10 @@
 package seoil.capstone.flashbid.domain.file.projection;
 
+import seoil.capstone.flashbid.global.common.enums.FileType;
+
 public interface FileProjection {
     Long getId();
     String getUrl();
+    Long getFileId();
+    FileType getFileType();
 }
