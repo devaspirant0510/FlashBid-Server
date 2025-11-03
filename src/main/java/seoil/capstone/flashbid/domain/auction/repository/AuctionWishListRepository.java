1d0fa8b8532879b9d780e0834444c09110bbe1ab
sylee0915 <seungyup8@gmail.com>
2025-11-04 03:43:59 +0900

    fix: 프로필페이지 관심상품 카운팅 정상화

--- src/main/java/seoil/capstone/flashbid/domain/auction/repository/AuctionWishListRepository.java
+++ src/main/java/seoil/capstone/flashbid/domain/auction/repository/AuctionWishListRepository.java
@@ -1,12 +1,18 @@
 package seoil.capstone.flashbid.domain.auction.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import seoil.capstone.flashbid.domain.auction.entity.AuctionWishListEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AuctionWishListRepository extends JpaRepository<AuctionWishListEntity,Long> {
     Optional<AuctionWishListEntity> findByUserIdAndAuctionId(Long userId, Long auctionId);
     boolean existsByUserIdAndAuctionId(Long userId, Long auctionId);
     void deleteByUserIdAndAuctionId(Long userId, Long auctionId);
+
+    @Query("SELECT awl FROM AuctionWishListEntity awl JOIN FETCH awl.auction WHERE awl.user.id = :userId")
+    List<AuctionWishListEntity> findAllByUserId(@Param("userId") Long userId);
 }
