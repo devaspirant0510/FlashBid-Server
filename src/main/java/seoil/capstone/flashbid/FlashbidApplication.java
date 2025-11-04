807f22278c9879195775dec66213d8db1546734a
devaspirant0510 <seungho020510@gmail.com>
2025-11-05 02:22:42 +0900

    feat: OpenFeign 및 UA Parser 라이브러리 추가

--- src/main/java/seoil/capstone/flashbid/FlashbidApplication.java
+++ src/main/java/seoil/capstone/flashbid/FlashbidApplication.java
@@ -2,12 +2,14 @@ package seoil.capstone.flashbid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.TimeZone;
 
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 @EnableAsync
 public class FlashbidApplication {
