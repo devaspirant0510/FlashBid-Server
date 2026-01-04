5b3de2f3bbc35b8ce49e27b1186ecd4b9c514633
devaspirant0510 <seungho020510@gmail.com>
2026-01-04 16:00:31 +0900

    feat: 카테고리 서비스 및 레포지토리 메서드 추가

--- src/main/java/seoil/capstone/flashbid/domain/category/repository/CategoryRepository.java
+++ src/main/java/seoil/capstone/flashbid/domain/category/repository/CategoryRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import seoil.capstone.flashbid.domain.category.entity.CategoryEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Long> {
     List<CategoryEntity> findAllByRootIdIsNull();
+    Optional<CategoryEntity> findByName(String name);
 }
